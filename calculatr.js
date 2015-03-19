567064e3146c077363fde56a77342ceb4606e873
fdaciuk <f.daciuk@gmail.com>
2015-03-19 13:17:55 -0300

    Add sum method

--- calculatr.js
+++ calculatr.js
@@ -3,6 +3,11 @@
 
 var program = require( 'commander' );
 var pkg = require( './package.json' );
-
+var sum = require( './lib/calculatr-sum' );
 program.version( pkg.version );
+program
+  .command( 'sum <number1> <number2>' )
+    .description( 'Sum two numbers' )
+    .action( sum );
+
 program.parse( process.argv );
--- test/test.calculatr.js
+++ test/test.calculatr.js
@@ -6,9 +6,19 @@ var calculatr = './calculatr.js';
 require( 'should' );
 
 describe( 'Calculatr', function() {
-    it( 'Should return version of calculatr', function() {
-        exec( calculatr + ' --version', function( err, stdout, stderr ) {
-            stdout.replace( '\n', '' ).should.be.equal( pkg.version );
-        });
+  it( 'Should return version of calculatr', function( done ) {
+    exec( calculatr + ' --version', function( err, stdout, stderr ) {
+      if( err ) throw err;
+      stdout.replace( '\n', '' ).should.be.equal( pkg.version );
+      done();
     });
+  });
+
+  it( 'Command "calculatr sum 1 2" Should return 3', function( done ) {
+    exec( calculatr + ' sum 1 2', function( err, stdout, stderr ) {
+      if( err ) throw err;
+      Number( stdout ).should.be.equal( 3 );
+      done();
+    });
+  });
 });
