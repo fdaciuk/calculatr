8e3d6ac1fcf2fa411b7c60d14832468b216db609
Fernando Daciuk <f.daciuk@gmail.com>
2015-03-21 01:05:14 +0000

    Improve structure

--- calculatr.js
+++ calculatr.js
@@ -4,7 +4,9 @@
 var program = require( 'commander' );
 var pkg = require( './package.json' );
 var sum = require( './lib/calculatr-sum' );
+
 program.version( pkg.version );
+
 program
   .command( 'sum <number1> <number2>' )
     .description( 'Sum two numbers' )
